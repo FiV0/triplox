6eccd57035652913e2411fd2e9bc2582d7df07be
Finn Völkel <finn.volkel@gmail.com>
2026-03-13 12:59:58 +0100

    Remove stuff

--- triplox-jvm/src/main/clojure/triplox/types.clj
+++ triplox-jvm/src/main/clojure/triplox/types.clj
@@ -1,6 +1,6 @@
 (ns triplox.types
   "Conversion between Triplox wire types and Clojure types."
-  (:import [java.util TreeMap]
+  (:import [java.util Map]
            [io.triplox.client DataTypeCodec$TaggedTuple]))
 
 (defn wire->clj
@@ -10,39 +10,18 @@
    Most types pass through unchanged."
   [v]
   (cond
-    (instance? TreeMap v)
+    (instance? Map v)
     (persistent!
      (reduce (fn [m entry]
                (assoc! m (keyword (.getKey ^java.util.Map$Entry entry))
                        (wire->clj (.getValue ^java.util.Map$Entry entry))))
              (transient {})
-             (.entrySet ^TreeMap v)))
+             (.entrySet ^Map v)))
 
     (instance? DataTypeCodec$TaggedTuple v)
     (mapv wire->clj (.elements ^DataTypeCodec$TaggedTuple v))
 
     (instance? java.util.List v)
     (mapv wire->clj v)
-
-    :else v))
-
-(defn clj->wire
-  "Convert a Clojure value to a wire protocol value.
-   Clojure map with keyword keys → TreeMap<String,Object>.
-   Most types pass through unchanged."
-  [v]
-  (cond
-    (map? v)
-    (let [tm (TreeMap.)]
-      (doseq [[k val] v]
-        (.put tm
-              (if (keyword? k)
-                (subs (str k) 1)
-                (str k))
-              (clj->wire val)))
-      tm)
-
-    (vector? v)
-    (java.util.ArrayList. ^java.util.Collection (mapv clj->wire v))
 
     :else v))
--- triplox-jvm/src/test/clojure/triplox/types_test.clj
+++ triplox-jvm/src/test/clojure/triplox/types_test.clj
@@ -35,29 +35,3 @@
   (testing "TaggedTuple → Clojure vector"
     (let [tuple (DataTypeCodec$TaggedTuple. (ArrayList. [42 true]))]
       (is (= [42 true] (types/wire->clj tuple))))))
-
-(deftest clj->wire-map
-  (testing "Clojure map → TreeMap with string keys"
-    (let [tm (types/clj->wire {:name "alice" :age 30})]
-      (is (instance? TreeMap tm))
-      (is (= "alice" (.get ^TreeMap tm "name")))
-      (is (= 30 (.get ^TreeMap tm "age"))))))
-
-(deftest clj->wire-vector
-  (testing "Clojure vector → ArrayList"
-    (let [al (types/clj->wire [1 2 3])]
-      (is (instance? ArrayList al))
-      (is (= [1 2 3] (vec al))))))
-
-(deftest clj->wire-primitives
-  (testing "Primitives pass through unchanged"
-    (is (= 42 (types/clj->wire 42)))
-    (is (= "hello" (types/clj->wire "hello")))
-    (is (= true (types/clj->wire true)))))
-
-(deftest roundtrip-map
-  (testing "Map round-trip: clj → wire → clj"
-    (let [original {:person/name "alice" :person/age 30}
-          wire (types/clj->wire original)
-          back (types/wire->clj wire)]
-      (is (= original back)))))
