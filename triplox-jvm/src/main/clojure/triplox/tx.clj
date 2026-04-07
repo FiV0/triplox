21fb296708cddd840df35f465d94a81d46f812d6
Finn Völkel <finn.volkel@gmail.com>
2026-04-07 10:25:37 +0200

    tweaks

--- triplox-jvm/src/main/clojure/triplox/tx.clj
+++ triplox-jvm/src/main/clojure/triplox/tx.clj
@@ -1,7 +1,6 @@
 (ns triplox.tx
   "Convert Datomic-style transaction data to TxOp objects."
-  (:import [io.triplox.client EntityRef EntityRef$Id EntityRef$TempId EntityRef$Ident
-            TxValue TxValue$Data TxValue$Ref
+  (:import [io.triplox.client EntityRef$Id EntityRef$TempId EntityRef$Ident TxValue$Data TxValue$Ref
             TxOp$Put TxOp$Add TxOp$Retract TxOp$Delete TxOp$Erase]))
 
 (defn- ->entity-ref
@@ -28,18 +27,13 @@
 
 (defn- vec->tx-op
   "Convert a Datomic-style tx-data vector to a TxOp."
-  [v]
-  (let [op (first v)]
-    (case op
-      :db/add     (let [[_ e a val] v]
-                    (TxOp$Add. (->entity-ref e) a (TxValue$Data. val)))
-      :db/retract (let [[_ e a val] v]
-                    (TxOp$Retract. (->entity-ref e) a (TxValue$Data. val)))
-      :db/delete  (let [[_ eid] v]
-                    (TxOp$Delete. (->entity-ref eid)))
-      :db/erase   (let [[_ eid] v]
-                    (TxOp$Erase. (->entity-ref eid)))
-      (throw (ex-info (str "Unknown tx-data op: " op) {:op op :form v})))))
+  [[op :as v]]
+  (case op
+    :db/add     (let [[_ e a val] v] (TxOp$Add. (->entity-ref e) a (TxValue$Data. val)))
+    :db/retract (let [[_ e a val] v] (TxOp$Retract. (->entity-ref e) a (TxValue$Data. val)))
+    :db/delete  (let [[_ eid] v] (TxOp$Delete. (->entity-ref eid)))
+    :db/erase   (let [[_ eid] v] (TxOp$Erase. (->entity-ref eid)))
+    (throw (ex-info (str "Unknown tx-data op: " op) {:op op :form v}))))
 
 (defn tx-data->ops
   "Convert a sequence of Datomic-style tx-data forms to a List<TxOp>.
