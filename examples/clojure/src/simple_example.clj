7963f4b81c602d2300b6e2c5fab24c246ed5f2a3
Finn Völkel <finn.volkel@gmail.com>
2026-04-09 13:27:54 +0200

    tweaks

--- examples/clojure/src/simple_example.clj
+++ examples/clojure/src/simple_example.clj
@@ -5,34 +5,34 @@
 
 (println (str "Connecting to " host ":" port "..."))
 
-(with-open [conn (tc/connect host port)]
-  (println "Connected.")
+(def conn (tc/connect host port))
 
-  ;; 1. Define schema attributes
-  (let [result (tc/transact conn [{:db/id 200 :db/ident :name
-                                   :db/valueType :db.type/string
-                                   :db/cardinality :db.cardinality/one}
-                                  {:db/id 201 :db/ident :age
-                                   :db/valueType :db.type/long
-                                   :db/cardinality :db.cardinality/one}])]
-    (if (:committed? result)
-      (println (str "Schema defined (tx_id=" (:tx-id result) ")."))
-      (throw (ex-info (str "Schema transaction aborted: " (:error-message result)) {}))))
+;; 1. Transact a schema
+(tc/transact conn [{:db/id 200
+                    :db/ident :name
+                    :db/valueType :db.type/string
+                    :db/cardinality :db.cardinality/one}
+                   {:db/id 201 :db/ident :age
+                    :db/valueType :db.type/long
+                    :db/cardinality :db.cardinality/one}])
+;; => {:tx-id 0,
+;;     :system-time 1775733871873835,
+;;     :committed? true,
+;;     :error-message nil}
 
-  ;; 2. Insert some data
-  (let [result (tc/transact conn [{:db/id 100 :name "alice" :age 30}
-                                  {:db/id 101 :name "bob" :age 25}])]
-    (if (:committed? result)
-      (println (str "Data inserted (tx_id=" (:tx-id result) ")."))
-      (throw (ex-info (str "Data transaction aborted: " (:error-message result)) {}))))
+;; 2. Transact some data
+(tc/transact conn [{:db/id 100 :name "alice" :age 30}
+                   {:db/id 101 :name "bob" :age 25}])
+;; => {:tx-id 1,
+;;     :system-time 1775733942779513,
+;;     :committed? true,
+;;     :error-message nil}
 
-  ;; 3. Open a DB snapshot and query
-  (with-open [db (tc/db conn)]
-    (let [rows (tc/q db '{:find [?e ?name ?age]
-                          :where [[?e :name ?name]
-                                  [?e :age ?age]]})]
-      (println (str "Query returned " (count rows) " row(s):"))
-      (doseq [row rows]
-        (println (str "  " (pr-str row))))))
 
-  (println "Done."))
+
+;; 3. Open a DB snapshot and query
+(with-open [db (tc/db conn)]
+  (tc/q db '{:find [?e ?name ?age]
+             :where [[?e :name ?name]
+                     [?e :age ?age]]}))
+;; => [[101 "bob" 25] [100 "alice" 30]]
