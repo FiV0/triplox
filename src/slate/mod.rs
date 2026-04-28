eb54529f1f38896c58258baed1fb737526e389d0
Finn Völkel <finn.volkel@gmail.com>
2026-04-28 16:35:24 +0200

    Add compactor None options for ingestion profiling

--- src/slate/mod.rs
+++ src/slate/mod.rs
@@ -127,6 +127,7 @@ pub async fn remote_slate(
         max_unflushed_bytes: 2 * 1024 * 1024 * 1024,
         l0_max_ssts: 16,
         wal_enabled: false,
+        // compactor_options: None,
         object_store_cache_options: ObjectStoreCacheOptions {
             root_folder: Some(cache_path.to_path_buf()),
             cache_puts: true,
