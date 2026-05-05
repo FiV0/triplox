3344e95f1340cf62246397291528ea48e8015fc3
Finn Völkel <finn.volkel@gmail.com>
2026-05-05 18:33:20 +0200

    comment tweak

--- triplox-client/src/lib.rs
+++ triplox-client/src/lib.rs
@@ -1,7 +1,7 @@
 //! HTTP/2 client for [Triplox](https://github.com/FiV0/triplox), a Datalog database.
 //!
 //! [`ClientNode`] mirrors the server's `Node` API and [`ClientDb`] mirrors the
-//! `DB` API, both operating over HTTP/2 via the binary protocol encoding.
+//! `DB` API, both operating over HTTP/2 with MessagePack encoding.
 
 pub mod client;
 pub mod msgpack_codec;
