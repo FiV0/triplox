ad9fc783172b58a5ad6ffd7a948638f360e44e3b
Finn Völkel <finn.volkel@gmail.com>
2026-06-09 12:27:33 +0200

    updates to dev/dev.clj

--- triplox-jvm/dev/dev.clj
+++ triplox-jvm/dev/dev.clj
@@ -4,18 +4,29 @@
 (comment
   (def conn (t/connect "localhost" 5490))
 
-  (t/transact conn [{:db/id 50 :db/ident :person/name :db/valueType :db.type/string :db/cardinality :db.cardinality/one}
-                    {:db/id 51 :db/ident :person/age :db/valueType :db.type/long :db/cardinality :db.cardinality/one}])
+  (t/transact conn [{:db/ident :person/name :db/valueType :db.type/string :db/cardinality :db.cardinality/one}
+                    {:db/ident :person/age :db/valueType :db.type/long :db/cardinality :db.cardinality/one}])
 
   ;; Transact some data
-  (t/transact conn [{:db/id 1001 :person/name "alice" :person/age 30}
-                    {:db/id 1002 :person/name "bob" :person/age 25}])
+  (t/transact conn [{:person/name "alice" :person/age 30}
+                    {:person/name "bob" :person/age 25}])
 
   ;; Open a DB value and query
   (def db (t/db conn))
   (t/q db '{:find [?name ?age]
             :where [[?e :person/name ?name]
                     [?e :person/age ?age]]})
+  ;; => [["bob" 25] ["alice" 30]]
+
+
+  (def sub (t/subscribe conn '{:find [?name ?age]
+                               :where [[?e :person/name ?name]
+                                       [?e :person/age ?age]]}))
+
+
+  (t/transact conn [{:person/name "eve" :person/age 99}])
+
+  (t/take! sub 1000)
 
   ;; Clean up
   (.close conn)
