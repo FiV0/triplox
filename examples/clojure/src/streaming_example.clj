5ce6b93725348488639416b89d0b23356f57fe5e
Finn Völkel <finn.volkel@gmail.com>
2026-09-09 20:56:06 +0200

    tweaks

--- examples/clojure/src/streaming_example.clj
+++ examples/clojure/src/streaming_example.clj
@@ -17,19 +17,12 @@
 ;;     :system-time
 ;;     #object[java.time.Instant 0x7c107312 "2026-09-02T12:31:24.534987Z"]}
 
-
-(tc/tx-key sub)
-;; => nil
-
 ;; Transact a name; the subscription receives a delta.
 ;; `take!` blocks for the next delta to arrive.
 (do
   (tc/transact conn [{:name "Ivan"}])
   (tc/take! sub))
 ;; => [[[8796093022208 "Ivan"] 1]]
-
-(tc/tx-key sub)
-;; => The transaction key of the delta returned by take! above.
 
 ;; The 2-arity bounds the wait and returns ::timeout when nothing changed.
 (tc/take! sub 200)
