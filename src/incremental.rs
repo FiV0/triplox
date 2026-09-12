2d01a1508ce36d5b017507e6afcf19e8edbb009b
Finn Völkel <finn.volkel@gmail.com>
2026-09-12 15:26:27 +0200

    don't use all cores for incremental queries

--- src/incremental.rs
+++ src/incremental.rs
@@ -48,7 +48,7 @@ impl Default for IncrementalQueryOptions {
     fn default() -> Self {
         Self {
             inbox_capacity: NonZeroUsize::new(256).unwrap(),
-            max_concurrent_steps: thread::available_parallelism().unwrap_or(NonZeroUsize::MIN),
+            max_concurrent_steps: thread::available_parallelism().unwrap_or(NonZeroUsize::MIN).div_ceil(NonZeroUsize::new(2).unwrap()),
             retire_timeout: Duration::from_secs(10),
         }
     }
